e3088ad69b6873904f2fb63279673740b3370910
praispranav <praispranav@gmail.com>
2022-12-02 15:43:19 +0530

    initial

---
+++ common-es2015.js
@@ -0,0 +1,17 @@
+(window["webpackJsonp"] = window["webpackJsonp"] || []).push([["common"],{
+
+/***/ "./src/assets/images/logo.png":
+/*!************************************!*\
+  !*** ./src/assets/images/logo.png ***!
+  \************************************/
+/*! exports provided: default */
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+__webpack_require__.r(__webpack_exports__);
+/* harmony default export */ __webpack_exports__["default"] = (__webpack_require__.p + "logo.png");
+
+/***/ })
+
+}]);
+//# sourceMappingURL=common-es2015.js.map
